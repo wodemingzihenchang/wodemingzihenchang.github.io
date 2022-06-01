1866346ee50eb86f2334d73600c54aeb4c1888a3
littleboy97 <735909224@qq.com>
2022-06-01 14:19:09 +0800

    Site updated: 2022-06-01 14:19:06

---
+++ Script.js
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+function elementvisibility(objid)
+{
+    var obj=document.getElementById(objid);
+    if(obj.style.visibility !='hidden')
+        {obj.style.visibility='hidden'}
+    else{obj.style.visibility='visible'}
+}
+function elementDisplay(objid)
+{
+    var obj=document.getElementById(objid);
+    obj.style.display = obj.style.display == 'none' ? 'block' : 'none';
+}
+</script>
+
+
