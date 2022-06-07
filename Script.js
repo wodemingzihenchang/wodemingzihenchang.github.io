ff95972e865592a1119ba33dd39a3241accbcc79
littleboy97 <735909224@qq.com>
2022-06-07 21:29:38 +0800

    Site updated: 2022-06-07 21:29:37

--- Script.js
+++ Script.js
@@ -1,16 +1,12 @@
 <script type="text/javascript">
-function elementvisibility(objid)
-{
-    var obj=document.getElementById(objid);
-    if(obj.style.visibility !='hidden')
-        {obj.style.visibility='hidden'}
-    else{obj.style.visibility='visible'}
-}
 function elementDisplay(objid)
 {
     var obj=document.getElementById(objid);
-    obj.style.display = obj.style.display == 'none' ? 'block' : 'none';
-}
-</script>
+    if(obj.style.visibility !='hidden')
+        {obj.style.visibility='hidden';}
+    else{obj.style.visibility='visible';}
+}</script>
 
-
+<div onclick="elementDisplay('shishi')">点击这里</div>
+    
+<div id="shishi" style="background-color:#000000;width:100px; left:100px; height:100px; position:absolute; "></div>
